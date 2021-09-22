d498f2c579512a86a0750ac2d24792e2a95b03d5
Victor Mihaita <victormihaita@artlandapp.com>
2021-09-22 10:24:35 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SFSafeSymbols",
-    platforms: [.iOS(.v11), .tvOS(.v11), .watchOS(.v6), .macOS(.v10_13)],
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v6), .macOS(.v10_13)],
     products: [
         .library(name: "SFSafeSymbols", type: .static, targets: ["SFSafeSymbols"]),
         .library(name: "SFSafeSymbols-Dynamic", type: .dynamic, targets: ["SFSafeSymbols"])
